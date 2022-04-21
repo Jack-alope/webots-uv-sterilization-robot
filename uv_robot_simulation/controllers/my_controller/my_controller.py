fb89e8503666a463b21949a4d47074c659385c89
Jack Murphy <jack@mrph.dev>
2022-04-21 14:16:20 +0100

    feat: object avoidance

--- uv_robot_simulation/controllers/my_controller/my_controller.py
+++ uv_robot_simulation/controllers/my_controller/my_controller.py
@@ -10,7 +10,9 @@ robot = Robot()
 
 waypoint = ((58,6), (55, 35), (15, 35), (15, 7))
 
+#waypoint = ((55, 35), (15, 35), (15, 7))
 
+#waypoint = ((58,6), (15, 35), (18, 7))
 occupancy_grid = pd.read_table('../../libraries/SampleTestMap1.csv', dtype=float, header=None, sep=',').fillna(0).values
 print(occupancy_grid[9][61])
 #To see what angle (in degrees) relitive to X axis bot is facing
@@ -26,23 +28,59 @@ def ang_to_X(xf,yf,xb,yb):
         return math.degrees(math.atan(yf/(xf - x_intercept)))
     
 def next_grid(curr_x_f, curr_y_f, curr_x_b, curr_y_b):
-    next_grid_x = int((curr_x_f - curr_x_b)/2) + curr_x_f
-    next_grid_y = int((curr_y_f - curr_y_b)/2) + curr_y_f
-    #print((next_grid_x, next_grid_y))
+    next_grid_x = int((curr_x_f - curr_x_b)/4) + curr_x_f
+    next_grid_y = int((curr_y_f - curr_y_b)/4) + curr_y_f
     
-    next_grid_x_r = int((curr_y_f - curr_y_b)/2) + curr_x_f
-    next_grid_y_r = int((curr_x_f - curr_x_b)/2) + curr_y_f
-    #print((next_grid_x_r, next_grid_y_r))
+    next_grid_x_r = int((curr_y_f - curr_y_b)/4) + curr_x_f
+    next_grid_y_r = int((curr_x_f - curr_x_b)/4) + curr_y_f
     
-    next_grid_x_l = -1*int((curr_y_f - curr_y_b)/2) + curr_x_f
-    next_grid_y_l = -1*int((curr_x_f - curr_x_b)/2) + curr_y_f
-    #print((next_grid_x_l, next_grid_y_l))
+    next_grid_x_l = -1*int((curr_y_f - curr_y_b)/4) + curr_x_f
+    next_grid_y_l = -1*int((curr_x_f - curr_x_b)/4) + curr_y_f
     
     occ1 = occupancy_grid[next_grid_y][next_grid_x]
     occ2 = occupancy_grid[next_grid_y_r][next_grid_x_r]
     occ3 = occupancy_grid[next_grid_y_l][next_grid_x_l]
-    return occ1+occ2+occ3
     
+    if occ1 or occ2 or occ3:
+        return True
+    
+    next_grid_x = int((curr_x_f - curr_x_b)/3) + curr_x_f
+    next_grid_y = int((curr_y_f - curr_y_b)/3) + curr_y_f
+    
+    next_grid_x_r = int((curr_y_f - curr_y_b)/3) + curr_x_f
+    next_grid_y_r = int((curr_x_f - curr_x_b)/3) + curr_y_f
+    
+    next_grid_x_l = -1*int((curr_y_f - curr_y_b)/3) + curr_x_f
+    next_grid_y_l = -1*int((curr_x_f - curr_x_b)/3) + curr_y_f
+    
+    occ1 = occupancy_grid[next_grid_y][next_grid_x]
+    occ2 = occupancy_grid[next_grid_y_r][next_grid_x_r]
+    occ3 = occupancy_grid[next_grid_y_l][next_grid_x_l]
+    
+    if occ1 or occ2 or occ3:
+        return True
+    return False
+
+def left_grid(curr_x_f, curr_y_f, curr_x_b, curr_y_b):
+    x = int((curr_grid_x_f - curr_grid_x_b)/4)
+    y = int((curr_grid_y_f - curr_grid_y_b)/4)
+    left_x = y + curr_grid_x_f
+    left_y = -1*x + curr_grid_y_f
+    
+    if occupancy_grid[left_y][left_x]:
+        return True
+        
+    x = int((curr_grid_x_f - curr_grid_x_b)/3)
+    y = int((curr_grid_y_f - curr_grid_y_b)/3)
+    left_x = y + curr_grid_x_f
+    left_y = -1*x + curr_grid_y_f
+    
+    if occupancy_grid[left_y][left_x]:
+        return True
+        
+        
+    return False
+     
 # Wheels 1 and 2 are front left and right wheels respectivly
 # Wheels 3 and 4 are back left and right wheels respectivly
 wheels = []
@@ -59,8 +97,6 @@ gps_front.enable(TIME_STEP)
 gps_back = robot.getGPS('gps_back')
 gps_back.enable(TIME_STEP) 
 
-last_move = 1
-made_contact = 0
 waypoints_hit = 0
 #Main code runs here
 while robot.step(TIME_STEP) != -1:
@@ -81,18 +117,22 @@ while robot.step(TIME_STEP) != -1:
     msg += str(ang)
     #print(msg)  
     
-    curr_grid_x_f = abs(math.floor(gps_front_value[0]/0.1))
+    curr_grid_x_f = abs(math.floor(gps_front_value[0]/0.1)) + 1
     curr_grid_y_f = abs(math.ceil(gps_front_value[1]/0.1))
 
-    curr_grid_x_b = abs(math.floor(gps_back_value[0]/0.1))
+    curr_grid_x_b = abs(math.floor(gps_back_value[0]/0.1)) + 1
     curr_grid_y_b = abs(math.ceil(gps_back_value[1]/0.1))
     
-    x = int((curr_grid_x_f - curr_grid_x_b)/2)
-    y = int((curr_grid_y_f - curr_grid_y_b)/2)
+    x = int((curr_grid_x_f - curr_grid_x_b)/4)
+    y = int((curr_grid_y_f - curr_grid_y_b)/4)
     left_x = y + curr_grid_x_f
-    left_y = x + curr_grid_y_f
-
+    left_y = -1*x + curr_grid_y_f
+    
+    object_in_way = next_grid(curr_grid_x_f, curr_grid_y_f, curr_grid_x_b, curr_grid_y_b)
+    object_to_left = left_grid (curr_grid_x_f, curr_grid_y_f, curr_grid_x_b, curr_grid_y_b)
+    
     waypoints_hit = waypoints_hit%len(waypoint)
+
     distance = math.sqrt((((waypoint[waypoints_hit][1]-curr_grid_y_f)**2)+((waypoint[waypoints_hit][0]-curr_grid_x_f))**2))
 
     expected_angle = math.asin((waypoint[waypoints_hit][1]-curr_grid_y_f)/distance)
@@ -124,15 +164,11 @@ while robot.step(TIME_STEP) != -1:
     else:
         norm_ang = ang
         
-    #print(expected_angle)
-    #print(expected_angle_2)
-    #print(waypoint[waypoints_hit])
-    #print(distance)
     if distance < 3:
        right_speed = 0
        left_speed = 0
        waypoints_hit = waypoints_hit+1
-    elif abs(norm_exp_ang - norm_ang) < 10:
+    elif abs(norm_exp_ang - norm_ang) < 8:
         left_speed = MAX_SPEED
         right_speed = MAX_SPEED
     elif math.sin(math.radians(norm_exp_ang - norm_ang)) > 0:
@@ -142,22 +178,14 @@ while robot.step(TIME_STEP) != -1:
         right_speed = -.2*MAX_SPEED
         left_speed = .2*MAX_SPEED
     
-
-
+    print(object_in_way)
+    if object_in_way:
+        right_speed = 0*MAX_SPEED
+        left_speed = MAX_SPEED
+    elif object_to_left:
+        left_speed = MAX_SPEED
+        right_speed = 0*MAX_SPEED
         
-    
-     #   made_contact = 1
-    #elif made_contact and not object_to_left:
-        #print('here')
-    #    left_speed = 0
-    #    right_speed = MAX_SPEED
-    #else:
-    #    left_speed = MAX_SPEED
-    #    right_speed = MAX_SPEED
-        
-    #else:
-    #    left_speed = .75* MAX_SPEED
-    #    right_speed = MAX_SPEED
            
     #Give speed to wheel motors  
     wheels[0].setVelocity(left_speed) 
