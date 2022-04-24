cb8410a4297d211e1a05717a1596d9a123376e90
Jack Murphy <jack@mrph.dev>
2022-04-24 17:53:13 +0100

    feat: right side avoidance

--- uv_robot_simulation/controllers/my_controller/my_controller.py
+++ uv_robot_simulation/controllers/my_controller/my_controller.py
@@ -9,6 +9,7 @@ MAX_SPEED = 6.28
 robot = Robot()
 
 waypoint = ((10,10), (55, 35), (33, 35), (10,35), (55,10), (33,10))
+
 occupancy_grid = pd.read_table('../../libraries/SampleTestMap1.csv', dtype=float, header=None, sep=',').fillna(0).values
 
 #To see what angle (in degrees) relitive to X axis bot is facing
@@ -38,11 +39,11 @@ def _is_object_in_way(curr_x_f, curr_y_f, curr_x_b, curr_y_b, start_scale):
 
     return False
     
-def _is_object_to_left(curr_x_f, curr_y_f, curr_x_b, curr_y_b, start_scale): 
+def _is_object_to_left(curr_x_f, curr_y_f, curr_x_b, curr_y_b, start_scale, right, dist): 
     x = int((curr_grid_x_f - curr_grid_x_b)/start_scale)
     y = int((curr_grid_y_f - curr_grid_y_b)/start_scale)
     left_x = y + curr_grid_x_f
-    left_y = -1*x + curr_grid_y_f
+    left_y = right*x + curr_grid_y_f
     
     if left_y > 40 or left_x > 60:
         return True
@@ -50,10 +51,11 @@ def _is_object_to_left(curr_x_f, curr_y_f, curr_x_b, curr_y_b, start_scale):
         return True
     elif occupancy_grid[left_y][left_x]:
         return True
-    elif start_scale > 1:
-        return _is_object_to_left(curr_x_f, curr_y_f, curr_x_b, curr_y_b, start_scale-1)        
+    elif start_scale > dist:
+        return _is_object_to_left(curr_x_f, curr_y_f, curr_x_b, curr_y_b, start_scale-1, right, dist)        
     
     return False
+
     
 # Wheels 1 and 2 are front left and right wheels respectivly
 # Wheels 3 and 4 are back left and right wheels respectivly
@@ -72,6 +74,7 @@ gps_back = robot.getGPS('gps_back')
 gps_back.enable(TIME_STEP) 
 
 waypoints_hit = 0
+last_left = True
 
 #Main code runs here
 while robot.step(TIME_STEP) != -1:
@@ -99,7 +102,8 @@ while robot.step(TIME_STEP) != -1:
     curr_grid_y_b = abs(math.ceil(gps_back_value[1]/0.1))
     
     object_in_way = _is_object_in_way(curr_grid_x_f, curr_grid_y_f, curr_grid_x_b, curr_grid_y_b, 5)
-    object_to_left = _is_object_to_left(curr_grid_x_f, curr_grid_y_f, curr_grid_x_b, curr_grid_y_b, 5)
+    object_to_left = _is_object_to_left(curr_grid_x_f, curr_grid_y_f, curr_grid_x_b, curr_grid_y_b, 5, -1, 1)
+    object_to_right = _is_object_to_left(curr_grid_x_f, curr_grid_y_f, curr_grid_x_b, curr_grid_y_b, 5, 1, 3)
     
     waypoints_hit = waypoints_hit%len(waypoint)
 
@@ -134,11 +138,12 @@ while robot.step(TIME_STEP) != -1:
     else:
         norm_ang = ang
         
-    if distance < 5:
+    # waypoint decision tree
+    if distance < 8:
        right_speed = 0
        left_speed = 0
-       waypoints_hit = waypoints_hit+1
-    elif abs(norm_exp_ang - norm_ang) < 8:
+       waypoints_hit = waypoints_hit + 1
+    elif abs(norm_exp_ang - norm_ang) < 5:
         left_speed = MAX_SPEED
         right_speed = MAX_SPEED
     elif math.sin(math.radians(norm_exp_ang - norm_ang)) > 0:
@@ -147,15 +152,17 @@ while robot.step(TIME_STEP) != -1:
     else:
         right_speed = -1*MAX_SPEED
         left_speed = MAX_SPEED
-    
-    print(object_in_way)
 
+    # object avoidance decision tree
     if object_in_way:
         right_speed = -.6*MAX_SPEED
         left_speed = MAX_SPEED
     elif object_to_left:
-        right_speed = .5*MAX_SPEED
+        right_speed = 0*MAX_SPEED
         left_speed = MAX_SPEED
+    elif object_to_right:
+        right_speed = MAX_SPEED
+        left_speed = 0*MAX_SPEED
         
            
     #Give speed to wheel motors  
